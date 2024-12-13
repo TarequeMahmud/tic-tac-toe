cedc35245f74138d17a8c8b5b60824457bd2e74b
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-12-13 15:34:37 +0600

    feat: add winner calculation logic for the game

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  //handleClick function
   function handleClick(i) {
-    if (squares[i]) return;
+    if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
     if (xIsNext) nextSquares[i] = "X";
     else nextSquares[i] = "O";
@@ -39,4 +40,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const linesToWin = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < linesToWin.length; i++) {
+    const [a, b, c] = linesToWin[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] && squares[c])
+      return squares[a];
+  }
+  return null;
 }
